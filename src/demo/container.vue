fed1902b4f643152e80bb2b975d6a65209c39f7a
chenjiawei <1084603453@qq.com>
2026-02-12 13:32:59 +0800

    feat：在线地图

--- src/demo/container.vue
+++ src/demo/container.vue
@@ -1,13 +1,15 @@
 <script setup lang="ts">
-import * as Cesium from 'cesium'
-import { ref } from 'vue'
-import 'cesium/Build/Cesium/Widgets/widgets.css'
+import * as Cesium from "cesium";
+import { ref } from "vue";
+import "cesium/Build/Cesium/Widgets/widgets.css";
 
 // @ts-expect-error - not in includes
-import { useViewerProvider } from '~composables/useViewer'
+import { useViewerProvider } from "~composables/useViewer";
 
-const container = ref<HTMLDivElement>()
+const container = ref<HTMLDivElement>();
 const { isMounted } = useViewerProvider(() => {
+  Cesium.Ion.defaultAccessToken =
+    "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJqdGkiOiJjNGRjM2M5YS0zYjM4LTRkNTItYWY2NS05ZWRmYWE0ODY4NzYiLCJpZCI6MzQxNzQwLCJpYXQiOjE3NTgwMTA2NTF9.PyBHExC-OzZi3arzqvBw6u1P2NhqIr3pqipPldjKR_8";
   const viewer = new Cesium.Viewer(container.value as HTMLDivElement, {
     geocoder: false,
     homeButton: false,
@@ -25,20 +27,20 @@ const { isMounted } = useViewerProvider(() => {
     /** Support high resolution screen */
     useBrowserRecommendedResolution: false,
     msaaSamples: 4,
-  })
+  });
 
   viewer.cesiumWidget.screenSpaceEventHandler.removeInputAction(
     Cesium.ScreenSpaceEventType.LEFT_DOUBLE_CLICK,
-  )
-  viewer.cesiumWidget.creditContainer.innerHTML = ''
-  viewer.scene.globe.depthTestAgainstTerrain = true
-  viewer.scene.screenSpaceCameraController.enableCollisionDetection = true
+  );
+  viewer.cesiumWidget.creditContainer.innerHTML = "";
+  viewer.scene.globe.depthTestAgainstTerrain = true;
+  viewer.scene.screenSpaceCameraController.enableCollisionDetection = true;
   viewer.scene.light = new Cesium.DirectionalLight({
     direction: new Cesium.Cartesian3(0.35, -0.918, -0.283358),
-  })
+  });
 
-  return viewer
-})
+  return viewer;
+});
 </script>
 
 <template>
